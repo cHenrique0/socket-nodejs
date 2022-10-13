c58fa0ed6b84b194327a2cfd7bb2deb35f0d6054
Claudio Henrique <claudio_henrique20@hotmail.com>
2022-10-13 18:54:29 -0300

    Adding payment modes

---
+++ server/paymentMode.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    id: 1,
+    mode: "Dinheiro",
+  },
+  {
+    id: 2,
+    mode: "Pix",
+  },
+  {
+    id: 3,
+    mode: "Débito",
+  },
+  {
+    id: 4,
+    mode: "Crédito",
+  },
+];
