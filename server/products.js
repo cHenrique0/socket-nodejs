1167b5f9f7170465f58dd24bb8773e049c7986cd
Claudio Henrique <claudio_henrique20@hotmail.com>
2022-10-13 18:54:10 -0300

    Adding products list

---
+++ server/products.js
@@ -0,0 +1,37 @@
+module.exports = [
+  {
+    id: 1,
+    name: "Banana",
+    price: 6.8,
+  },
+  {
+    id: 2,
+    name: "Uva",
+    price: 8.4,
+  },
+  {
+    id: 3,
+    name: "Morango",
+    price: 7.4,
+  },
+  {
+    id: 4,
+    name: "Pão",
+    price: 3.2,
+  },
+  {
+    id: 5,
+    name: "Biscoito",
+    price: 2.2,
+  },
+  {
+    id: 6,
+    name: "Refrigerante",
+    price: 2.2,
+  },
+  {
+    id: 7,
+    name: "Queijo",
+    price: 15.2,
+  },
+];
